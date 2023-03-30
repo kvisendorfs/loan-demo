61d1936fd60abe3f30f98ea41a96389efea92df0
Kārlis Visendorfs <karlis.visendorfs@proofit.lv>
2023-03-30 23:05:19 +0300

    Initialize project

---
+++ src/test/java/lv/kvisendorfs/loandemo/LoanDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.kvisendorfs.loandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
