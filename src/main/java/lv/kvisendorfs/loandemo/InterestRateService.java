6e53e5c908e7334a6c0087604a6b065b8181d327
Kārlis Visendorfs <karlis.visendorfs@proofit.lv>
2023-04-03 19:46:32 +0300

    Extract rate to configuration
    Use spring boot yaml configuration and add unit tests for service

--- src/main/java/lv/kvisendorfs/loandemo/InterestRateService.java
+++ src/main/java/lv/kvisendorfs/loandemo/InterestRateService.java
@@ -2,17 +2,23 @@ package lv.kvisendorfs.loandemo;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InterestRateService {
 
-	//TODO tests
+	@Value("${rates.housing}")
+	protected BigDecimal housingLoanInterestRate;
+
 	public BigDecimal getInterestRateForLoanType(LoanType loanType) {
+		if (loanType == null) {
+			throw new NoInterestRateException();
+		}
 		BigDecimal interestRate;
 		switch (loanType) {
 		case HOUSING:
-			interestRate = new BigDecimal("0.035");
+			interestRate = housingLoanInterestRate;
 			break;
 		default:
 			throw new NoInterestRateException();
---
+++ src/test/java/lv/kvisendorfs/loandemo/InterestRateServiceTest.java
@@ -0,0 +1,38 @@
+package lv.kvisendorfs.loandemo;
+
+import static lv.kvisendorfs.loandemo.LoanType.HOUSING;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class InterestRateServiceTest {
+
+	private static final String DEFAULT_RATE = "0.035";
+
+	@Autowired
+	private InterestRateService rateService;
+
+	@BeforeEach
+	public void setUp() {
+		rateService = new InterestRateService();
+		rateService.housingLoanInterestRate = new BigDecimal(DEFAULT_RATE);
+	}
+
+	@Test
+	public void testDefaultConfiguration() {
+		var rate = rateService.getInterestRateForLoanType(HOUSING);
+		assertThat(rate).isEqualByComparingTo(DEFAULT_RATE);
+	}
+
+	@Test
+	public void testNull() {
+		assertThatThrownBy(() -> rateService.getInterestRateForLoanType(null))
+				.isInstanceOf(NoInterestRateException.class);
+	}
+
+}
